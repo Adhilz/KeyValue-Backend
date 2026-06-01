998661516f6c0aa5b51b34e22c77dafe619504e2
Adhilz <adhilsalam200@gmail.com>
2026-06-01 17:17:45 +0530

    Containerized

--- auth/router.py
+++ auth/router.py
@@ -6,11 +6,13 @@ from auth.schemas import TokenResponse,LoginRequest
 from database.connection import get_db
 from employees import services as employee_service
 from fastapi.security import OAuth2PasswordRequestForm
-
+import logging
+logger=logging.getLogger(__name__)
 router= APIRouter(prefix="/auth",tags=["Auth"])
 
 @router.post("/login",response_model=TokenResponse)
 async def login(form:OAuth2PasswordRequestForm =Depends(),db: Session =Depends(get_db) ):
     token= await auth_service.login(db,form.username,form.password)
+    logger.info(f"User {form.username} logged in successfully")
     return TokenResponse(access_token=token)
 
